b1dbb7c6a6787e89e9d992452007369e7ed61dd7
itcast-hsian <541510140@qq.com>
2019-09-18 13:56:15 +0800

    i want2 sleep

--- src/middleware/authorize.ts
+++ src/middleware/authorize.ts
@@ -7,23 +7,28 @@ const authorize = (strict: Boolean = true) => {
     return (context: any,  key: string, desc: PropertyDescriptor) => {
 
         context[key].authorize = (ctx: BaseContext, next: any) => {
-            const token = ctx.request.headers['authorization'];
-            if(strict === false){
-                if(token){
-                    ctx.state.user = verify(token.replace('Bearer ', ''), TOKEN_SECRET);
-                }
-                return next();
-            }
-
-            // default
-            if(strict === true){
-                if(token){
-                    ctx.state.user = verify(token.replace('Bearer ', ''), TOKEN_SECRET);
+            try{
+                const token = ctx.request.headers['authorization'];
+                if(strict === false){
+                    if(token){
+                        ctx.state.user = verify(token.replace('Bearer ', ''), TOKEN_SECRET);
+                    }
                     return next();
                 }
-            }
 
-            return ctx.body = new Exception(401, "用户信息验证失败").toObject();
+                // default
+                if(strict === true){
+                    if(token){
+                        ctx.state.user = verify(token.replace('Bearer ', ''), TOKEN_SECRET);
+                        return next();
+                    }
+                }
+
+                return ctx.body = new Exception(401, "用户信息验证失败").toObject();
+            }catch(err){
+                console.log(err);
+                return ctx.body = new Exception(401, "用户信息验证失败").toObject();
+            }
         }
     } 
 }
